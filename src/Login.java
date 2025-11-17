9406fd659b0b2de056556857df4c7a2fb66c4596
Mirza-Selimovic06 <mselimovic.8@gmail.com>
2025-11-16 21:27:54 -0500

    tried making frame

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,13 @@
-public class Login {
-    void login(){
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
+public class Login extends JFrame{
+    void login(){
+        ImageIcon image = new ImageIcon("logo.jpg");
+        this.setTitle("Login Page");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit of the application when hitting the X button on top right
+        this.setSize(400, 400); //Set the size of the login page frame
+        this.setVisible(true); //Sets visabi lity to true
+        this.setIconImage(image.getImage());
     }
 }
